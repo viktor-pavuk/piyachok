0f82d06a13f89107c648340c748001c3e0e8fd0f
ViktorPavukNULP <72486611+ViktorPavukNULP@users.noreply.github.com>
2022-07-05 11:29:40 +0300

    user and profile validation

--- apps/profile/models.py
+++ apps/profile/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.validators import RegexValidator
 from django.db import models
 
 UserModel = get_user_model()
@@ -8,8 +9,17 @@ class ProfileModel(models.Model):
     class Meta:
         db_table = 'profile'
 
-    name = models.CharField(max_length=30)
-    surname = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, validators=(
+        RegexValidator(r'^([A-Z]{1}[a-z]{1,19})$',
+                       'Name must start with the capital letter and contain from 2 to 20 letters. Example: Andriy'),
+    ))
+    surname = models.CharField(max_length=30, validators=(
+        RegexValidator(r'^([A-Z]{1}[a-z]{1,19})$',
+                       'Surname must start with the capital letter and contain from 2 to 20 letters. Example: Popov'),
+    ))
     born = models.DateField()
-    phone = models.CharField(max_length=13, unique=True)
+    phone = models.CharField(max_length=13, validators=(
+        RegexValidator(r'^\+380[\d]{9}$', 'Invalid phone number. Example: +380xxxxxxxxx'),
+    ))
+    # avatar = models.ImageField(upload_to=AvatarUtils.upload_to, blank=True)
     user = models.OneToOneField(UserModel, on_delete=models.CASCADE, related_name='profile')
--- apps/user/models.py
+++ apps/user/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.core.validators import RegexValidator
 from django.db import models
 
 from .managers import UserManager
@@ -9,6 +10,13 @@ class UserModel(AbstractBaseUser, PermissionsMixin):
         db_table = 'auth_user'
 
     email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=128, validators=(
+        RegexValidator(
+            r'^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\s])[^\s]{8,20}$',
+            'Password must contain at least 8 characters: 1 num, 1 uppercase letter, 1 lowercase letter, '
+            '1 non-alpha-numeric '
+        ),
+    ))
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
