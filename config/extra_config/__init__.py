557366473e882bb3752354a3ba5c545cde59f277
ViktorPavukNULP <72486611+ViktorPavukNULP@users.noreply.github.com>
2022-07-14 15:49:48 +0300

    templates

--- config/extra_config/__init__.py
+++ config/extra_config/__init__.py
@@ -1,2 +1,3 @@
+from .email_config import *
 from .jwt_config import *
 from .rest_config import *
---
+++ utils/email_util.py
@@ -0,0 +1,19 @@
+import os
+
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
+
+
+class EmailUtils:
+    @staticmethod
+    def _send_email(to: str, template_name: str, context: dict, subject='') -> None:
+        template = get_template(template_name)
+        html_content = template.render(context)
+        msg = EmailMultiAlternatives(subject, from_email=os.environ.get('EMAIL_HOST'), to=[to])
+        msg.attach_alternative(html_content, 'text/html')
+        msg.send()
+
+    @classmethod
+    def register_email(cls, address: str, name: str, token: str) -> None:
+        url = f'{os.environ.get("FRONTEND_HOST")}activate/{token}/'
+        cls._send_email(address, 'register_email.html', {'name': name, 'link': url})
